1df299b7fcfc4d013563156667216d0aa8b4100b
Hans Windisch <hans-michael.windisch@thi.de>
2025-11-24 14:25:00 +0100

    Angular demo1

---
+++ 04-Angular/demo1/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Hello } from "./hello/hello";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Hello],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'demo1';
+}
---
+++ 04-Angular/demo1/src/app/hello/hello.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.css',
+})
+export class Hello {
+  name: string = 'Anna';
+
+  changeName(): void {
+    this.name += '::' + this.name;
+  }
+}
