0102bca510e372baee526db97d5ddd32a482e22c
Hans Windisch <hans-michael.windisch@thi.de>
2025-12-01 11:48:04 +0100

    update

--- 04-Angular/signal-demos/computed.ts
+++ 04-Angular/signal-demos/computed.ts
@@ -1,6 +1,7 @@
 import { signal, computed } from '@angular/core';
 
 const count = signal(0);
+// arrow function is called whenever "count" changes
 const doubleCount = computed(() => count() * 2);
 
 console.log(doubleCount());  // 0
