d03741a8f6a00322483773e80255556fa0c4d192
Hans Windisch <hans-michael.windisch@thi.de>
2025-12-09 16:34:32 +0100

    update

--- 05-React/todo-list-exercise/src/AddTodoForm.js
+++ 05-React/todo-list-exercise/src/AddTodoForm.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import './AddTodoForm.css';
 
 function AddTodoForm({ todos, setTodos }) {
   const [description, setDescription] = useState('');
-  
+  const [classname, setClassname] = useState('');
+  const [disabled, setDisabled] = useState('true');
+
   const changeDescription = (e) => {
     const value = e.target.value;
     setDescription(value);
+    setClassname('');
+    setDisabled('');
+    if (value.trim() === '') {
+      setClassname('error');
+      setDisabled('true');
+    }
   }
 
   const add = () => {
+    if (description.trim() === '') {
+      return;
+    }
     const newTodo = {
       id: Date.now(),
       description: description,
@@ -17,6 +29,7 @@ function AddTodoForm({ todos, setTodos }) {
 
     setTodos([...todos, newTodo]);
     setDescription('');
+    setDisabled('true');
   }
 
   return (
@@ -26,8 +39,9 @@ function AddTodoForm({ todos, setTodos }) {
         placeholder="describe todo..." 
         onChange={changeDescription}
         value={description}
+        className={classname}
       />
-      <button onClick={add}>add</button>
+      <button onClick={add} disabled={disabled}>add</button>
     </>
   );
 }
